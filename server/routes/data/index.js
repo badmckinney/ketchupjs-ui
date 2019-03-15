2110cf72f07447ea4dde1d245afc78458d7ea5b4
Brad McKinney <brad.mckin@gmail.com>
2019-03-15 10:55:49 -1000

    write backend routes for API

--- server/routes/data/index.js
+++ server/routes/data/index.js
@@ -7,7 +7,7 @@ const Event = require('../../../database/models/Event');
  *  GET
  ************************/
 
-router.get('/dashboard', (req, res) => {
+router.get('/profile', (req, res) => {
   const id = req.user.id;
 
   Client.where('id', id)
@@ -19,4 +19,58 @@ router.get('/dashboard', (req, res) => {
     .catch(err => {
       res.status(500).json(err);
     });
-}); 
+});
+
+router.get('/names', (req, res) => {
+  Client.fetchAll({ columns: ['name'] })
+    .then(names => res.json({ names: names }))
+    .catch(err => res.status(500).json(err));
+});
+
+router.get('/feature', (req, res) => {
+  Client.fetchAll({ columns: ['id'] })
+    .then(ids => {
+      feature = ids.slice(Math.floor(Math.random * ids.length + 1), 1);
+      Event.where('client_id', feature)
+        .fetchAll({ withRelated: ['client'] })
+        .then(events => res.json({ events: events }))
+        .catch(err => res.status(500).json(err));
+    });
+});
+
+router.get('/:client', (req, res) => {
+  const client = decodeURIComponent(req.params.client);
+  Client.where({ name: client })
+    .fetchAll({ withRelated: ['events'] })
+    .then(client => res.json({ client: client }))
+    .catch(err => res.status(500).json(err));
+});
+
+/************************
+ * PUT
+ ************************/
+
+router.put('/profile', (req, res) => {
+  const id = req.user.id;
+  const client = req.body;
+
+  const updatedInfo = {
+    name: client.name,
+    username: client.username,
+    public: client.public
+  }
+
+  Client.where('id', id)
+    .fetch()
+    .then(client => {
+      if (!client) { res.status(400).json({ error: 'Account not found' }) }
+      if (client.attributes.id !== id) { res.status(400).json({ error: Unauthorized }) }
+
+      client
+        .save(updatedInfo, { patch: true })
+        .then(() => res.json({ success: true }))
+        .catch(err => res.status(500).json(err));
+    });
+});
+
+module.exports = router;
