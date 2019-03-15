ab0347e7ce1d6eb3779b675f79353779dc166d69
Brad McKinney <brad.mckin@gmail.com>
2019-03-14 16:07:35 -1000

    build out react bare bones

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReduxThunk from 'redux-thunk';
+import './index.scss';
+import App from './containers/App';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension's options like name, actionsBlacklist, actionsCreators, serialize...
+    })
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(ReduxThunk));
+
+const store = createStore(itemReducer, enhancer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
